e40c27f22656b07e2b8864e11dcde7c4c6732b0f
becca <r.alexis.tolentino@gmail.com>
2017-10-10 14:15:28 -0500

    entered album data into codeup test db

---
+++ albums_seeder.sql
@@ -0,0 +1,8 @@
+USE codeup_test_db;
+
+INSERT INTO albums(artist, name,release_date, sales, genre)
+VALUES('Michael Jackson','Thriller',1982,47.3, 'pop,rock,R&B'),
+('AC/DC', 'Back in Black',1980,47.3,'Hard Rock'),
+('Pink Floyd','The Dark Side Of The Moon', 1973, 24.2,'Progressive rock'),
+('Whitney Houston','The Bodyguard',1992,27.4,'R&B,Soul,Pop,Soundtrack'),
+('Meat Loaf','Bat Out Of Hell',1977,21.5,'Hard rock,Progressive rock');
