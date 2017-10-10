0f7d4a11e406dabaf3ebec6d350b98f90585cfc2
becca <r.alexis.tolentino@gmail.com>
2017-10-10 11:25:58 -0500

    created albums table using mysql

---
+++ albums_migration.sql
@@ -0,0 +1,13 @@
+USE codeup_test_db;
+
+DROP TABLE IF EXISTS albums;
+
+CREATE TABLE albums(
+id    INT UNSIGNED NOT NULL AUTO_INCREMENT,
+artist  VARCHAR(150) NOT NULL,
+NAME  VARCHAR(150) NOT NULL,
+release_date  DATE,
+sales DECIMAL(10, 2),
+genre VARCHAR(50),
+PRIMARY KEY(id)
+);
