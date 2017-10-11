4c57fcb47cdc1fe766d288bf5be3f6607e67cb77
becca <r.alexis.tolentino@gmail.com>
2017-10-11 11:01:52 -0500

    cleaned up

--- albums_seeder.sql
+++ albums_seeder.sql
@@ -5,20 +5,20 @@ VALUES('Michael Jackson','Thriller',1982,47.3, 'pop,rock,R&B'),
 ('AC/DC', 'Back in Black',1980,47.3,'Hard Rock'),
 ('Pink Floyd','The Dark Side Of The Moon', 1973, 24.2,'Progressive rock'),
 ('Whitney Houston','The Bodyguard',1992,27.4,'R&B,Soul,Pop,Soundtrack'),
-('Meat Loaf','Bat Out Of Hell',1977,21.5,'Hard rock,Progressive rock')
-('Eagles','Their Greatest Hits', 1976,32.2,'Rock,Soft rock,Folk,rock')
-('Bee Gees','Saturday Night Fever',1977, 20.6,'Disco')
-('Fleetwood Mac','Rumors',1977, 27.9,'Soft rock')
-('Shania Twain','Come On Over',1997,29.6,'Country Pop')
-('Led Zeppelin','Led Zeppelin IV',1971,29.0,'Hard rock,Heavy Metal Folk')
-('Michael Jackson','Bad',1987,23.6,'Pop,Funk,Rock')
-('Alanis Morissette','Jagged Little Pill',1995,24.4,'Alternative rock')
-('Celine Dion','Falling into You',1996,20.2,'Pop,Soft rock')
-('The Beatles', 'Sgt.Pepper''s Lonely Hearts Club Band', 1967,13.1,'Rock')
-('Michael Jackson','Dangerous',1991,19.4,'Rock,Funk,Pop')
-('Eagles','Hotel California',1976,21.5,'Rock')
-('Various artists','Dirty Dancing',1987,17.9,'Pop,Rock,R&B')
-('Adele','21',2011, 25.3,'Pop,Soul')
+('Meat Loaf','Bat Out Of Hell',1977,21.5,'Hard rock,Progressive rock'),
+('Eagles','Their Greatest Hits', 1976,32.2,'Rock,Soft rock,Folk,rock'),
+('Bee Gees','Saturday Night Fever',1977, 20.6,'Disco'),
+('Fleetwood Mac','Rumors',1977, 27.9,'Soft rock'),
+('Shania Twain','Come On Over',1997,29.6,'Country Pop'),
+('Led Zeppelin','Led Zeppelin IV',1971,29.0,'Hard rock,Heavy Metal Folk'),
+('Michael Jackson','Bad',1987,23.6,'Pop,Funk,Rock'),
+('Alanis Morissette','Jagged Little Pill',1995,24.4,'Alternative rock'),
+('Celine Dion','Falling into You',1996,20.2,'Pop,Soft rock'),
+('The Beatles', 'Sgt.Pepper''s Lonely Hearts Club Band', 1967,13.1,'Rock'),
+('Michael Jackson','Dangerous',1991,19.4,'Rock,Funk,Pop'),
+('Eagles','Hotel California',1976,21.5,'Rock'),
+('Various artists','Dirty Dancing',1987,17.9,'Pop,Rock,R&B'),
+('Adele','21',2011, 25.3,'Pop,Soul'),
 ('Celine Dion','Let''s Talk About Love',1997,19.3,'Pop,Soft rock'),
 ('The Beatles','1',2000,22.6,'Rock'),
 ('Madonna','The Immaculate Collection',1990,19.4,'Pop,Dance'),
--- select_exercise.sql
+++ select_exercise.sql
@@ -1,32 +1,21 @@
-USE codeu_test_db;
-select'';
+USE codeup_test_db;
 
-select'all albums by Pink Floyd' AS 'info';
-Select name from albums where artist = 'Pink Floyd';
+select'all albums by Pink Floyd' AS '';
+Select name as '' from albums where artist = 'Pink Floyd';
 
-select'';
+select 'the year Sgt. Pepper''s Lonely Hearts Club band was released' AS '';
+select release_date as '' from albums where name = 'Sgt. Pepper''s Lonely Hearts Club Band';
 
-select 'the year Sgt. Pepper''s Lonely Hearts Club band was released' AS 'info';
-select year from albums where name = 'Sgt. Pepper''s Lonely Hearts Club Band';
+select 'The genre for Nevermind'AS '';
+select genre as '' from albums where name = 'Nevermind';
 
-select'';
+select 'Which albums were released in the 1990''s' AS '';
+select name as '' from albums where release_date  between 1990 and 1999;
 
-select 'The genre for Nevermind'AS 'info';
-select genre from albums where name = 'Nevermind';
+select 'Which albums had less than 20 million certified sales' AS '';
+select name as '' from albums where sales < 20;
 
-select'';
-
-select 'Which albums were released in the 1990''s' AS 'info';
-select release_date from albums where release_date  between = 1990 and 1999;
-
-select'';
-
-select 'Which albums had less than 20 million certified sales' AS 'info';
-select sales from albums where sales between = 10 and 19.9;
-
-select'';
-
-select 'albums with the a genre of rock' AS 'info';
-select genre from albums where genre = 'rock';
+select 'albums with the a genre of rock' AS '';
+select name as '' from albums where genre = 'rock';
 
 
