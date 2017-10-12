4a16598b3ad17281e9154ea58d0e163f75839065
becca <r.alexis.tolentino@gmail.com>
2017-10-12 15:14:46 -0500

    limit exercise mysql setting limits to see our data results in batches

---
+++ limit_exercises.sql
@@ -0,0 +1,15 @@
+USE employees;
+
+select distinct last_name
+from employees
+where last_name like 'Z%'
+order by last_name desc
+limit 10 offset 0;
+
+select *
+from employees
+where hire_date like '199%'
+AND birth_date like '%-12-25'
+order by birth_date, hire_date desc
+limit 5 offset 0;
+
